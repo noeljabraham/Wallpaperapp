c26cf019d7a947db3c911b92655f4da242c42099
noelj <noeljabraham35@gmail.com>
2023-05-18 23:14:26 +0530

    after infinite scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(WallpaperApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class WallpaperApp extends StatefulWidget {
   @override
+  _WallpaperAppState createState() => _WallpaperAppState();
+}
 
+class _WallpaperAppState extends State<WallpaperApp> {
+  final TextEditingController _searchController = TextEditingController();
+  String searchQuery = '';
+  List<String> wallpapers = [];
+  int currentPage = 1;
+  int totalPages = 1;
+  bool isLoading = false;
 
-  eff
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(onSearchQueryChanged);
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void onSearchQueryChanged() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      searchQuery = _searchController.text;
+      currentPage = 1;
+      totalPages = 1;
+      wallpapers.clear();
+    });
+    searchWallpapers();
+  }
+
+  void searchWallpapers() async {
+    if (isLoading || currentPage > totalPages) return;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final results = await fetchWallpapers(searchQuery, currentPage);
+
+    setState(() {
+      wallpapers.addAll(results);
+      currentPage++;
+      isLoading = false;
     });
   }
 
+  Future<List<String>> fetchWallpapers(String searchQuery, int page) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://api.unsplash.com/search/photos?query=$searchQuery&page=$page'),
+      headers: {
+        'Authorization':
+            'Client-ID RqkdzJ6EJTL-A_4EM7r6DYpEUg16BcH0qJ3Z0oi4TuY',
+      },
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<dynamic> photos = data['results'];
+      totalPages = data['total_pages'];
+      return photos.map<String>((photo) => photo['urls']['regular']).toList();
+    } else {
+      throw Exception('Failed to fetch wallpapers');
+    }
+  }
+
+  Widget buildWallpaperGrid() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 4.0,
+      ),
+      itemCount: wallpapers.length + 1, // +1 for the loading indicator
+      itemBuilder: (BuildContext context, int index) {
+        if (index == wallpapers.length) {
+          if (currentPage <= totalPages) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return SizedBox.shrink();
+          }
+        } else {
+          return GestureDetector(
+            onTap: () {
+              // Implement wallpaper details screen or download functionality
+              print('Downloading wallpaper: ${wallpapers[index]}');
+            },
+            child: Image.network(
+              wallpapers[index],
+              fit: BoxFit.cover,
+            ),
+          );
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      title: 'Wallpaper App',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Wallpaper App'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        labelText: 'Search Wallpaper',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      onSearchQueryChanged();
+                    },
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: wallpapers.isEmpty
+                  ? Center(child: Text('No wallpapers to display'))
+                  : NotificationListener<ScrollNotification>(
+                      onNotification: (scrollNotification) {
+                        if (scrollNotification is ScrollEndNotification) {
+                          final metrics = scrollNotification.metrics;
+                          if (metrics.atEdge && metrics.pixels != 0) {
+                            searchWallpapers();
+                          }
+                        }
+                        return false;
+                      },
+                      child: buildWallpaperGrid(),
+                    ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
